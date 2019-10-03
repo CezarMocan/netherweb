34912f2134e68c6a228921fe75481504d4c44f5f
Cezar Mocan <mocancezar@gmail.com>
2019-10-03 14:01:15 -0400

    Emitting mouse events

--- pages/questions.js
+++ pages/questions.js
@@ -13,16 +13,16 @@ const QUESTIONS = [
         { text: 'What would constitute a “perfect” day for you?' },        
     ],
     [
-        { text: '5' },
-        { text: '6' },
-        { text: '7' },
-        { text: '8' },        
+        { text: 'When did you last sing to yourself? To someone else?' },
+        { text: 'Do you have a secret hunch about how you will die?' },
+        { text: 'For what in your life do you feel most grateful?' },
+        { text: 'If you could change anything about the way you were raised, what would it be?' },        
     ],
     [
-        { text: '9' },
-        { text: '10' },
-        { text: '11' },
-        { text: '12' },        
+        { text: 'Is there something that you’ve dreamed of doing for a long time? Why haven’t you done it?' },
+        { text: 'What is the greatest accomplishment of your life? ' },
+        { text: 'What do you value most in a friendship?' },
+        { text: 'If you knew that in one year you would die suddenly, would you change anything about the way you are now living? Why?' },        
     ]
 ]
 
@@ -55,7 +55,7 @@ export default class Index extends React.Component {
                             { row.map(el => {
                                 return (
                                     <div className="questions-element-container">
-                                        {el.text}
+                                        <div className="question-element">{el.text}</div>
                                     </div>
                                 )
                             })}
